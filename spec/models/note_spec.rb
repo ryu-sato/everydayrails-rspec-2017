43ab0f49ea55dc559950e675508b6dd92e58fd72
Ryu Sato <ryu@weseek.co.jp>
2019-07-14 23:59:23 +0900

    検索できない場合のケースをテストする

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -30,4 +30,33 @@ RSpec.describe Note, type: :model do
     expect(Note.search('first')).to include(note1, note3)
     expect(Note.search('first')).not_to include(note2)
   end
+
+  # 検索結果が1件も見付からなければ空のコレクションを返すこと
+  it 'returns an empty collection when no results are found' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name:  'Tester',
+      email:      'joetester@example.com',
+      password:   'dottle-nouveau-pavillion-tights-furze'
+    )
+
+    project = user.projects.create(
+      name: 'Test Project'
+    )
+
+    note1 = project.notes.create(
+      message: 'This is the first note.',
+      user: user
+    )
+    note2 = project.notes.create(
+      message: 'This is the second note.',
+      user: user
+    )
+    note3 = project.notes.create(
+      message: 'First, preheat the oven.',
+      user: user
+    )
+
+    expect(Note.search('message')).to be_empty
+  end
 end
