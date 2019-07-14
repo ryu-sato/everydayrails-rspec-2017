e9c639946f32957383223331504795a7c2e027ad
Ryu Sato <ryu@weseek.co.jp>
2019-07-14 23:51:53 +0900

    note model を追加

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  # 検索文字列に一致するメモを返すこと
+  it 'returns notes that match the search term' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name:  'Tester',
+      email:      'joetester@example.com',
+      password:   'dottle-nouveau-pavillion-tights-furze'
+    )
+
+    project = user.projects.create(
+      name: 'Test Project'
+    )
+
+    note1 = project.notes.create(
+      message: 'This is the first note.',
+      user: user
+    )
+    note2 = project.notes.create(
+      message: 'This is the second note.',
+      user: user
+    )
+    note3 = project.notes.create(
+      message: 'First, preheat the oven.',
+      user: user
+    )
+
+    expect(Note.search('first')).to include(note1, note3)
+    expect(Note.search('first')).not_to include(note2)
+  end
+end
