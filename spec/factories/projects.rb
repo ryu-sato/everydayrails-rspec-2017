a18f9208788120ee363412db59cc158e7bc39a20
Ryu Sato <ryu@weseek.co.jp>
2019-07-16 00:42:38 +0900

    FactoryBot の継承を使って DRY にした

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,29 +4,20 @@ FactoryBot.define do
     description 'A test project.'
     due_on 1.week.from_now
     association :owner
-  end
 
-  # 昨日が締め切りのプロジェクト
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
-    due_on 1.day.ago
-    association :owner
-  end
+    # 昨日が締め切りのプロジェクト
+    factory :project_due_yesterday do
+      due_on 1.day.ago
+    end
 
-  # 今日が締め切りのプロジェクト
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
-    due_on Date.current.in_time_zone
-    association :owner
-  end
+    # 今日が締め切りのプロジェクト
+    factory :project_due_today do
+      due_on Date.current.in_time_zone
+    end
 
-  # 明日が締め切りのプロジェクト
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Project #{n}" }
-    description 'A test project.'
-    due_on 1.day.from_now
-    association :owner
+    # 明日が締め切りのプロジェクト
+    factory :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
