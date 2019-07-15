d49cb3a55924813a6921242fde2ee5d3943da510
Ryu Sato <ryu@weseek.co.jp>
2019-07-16 00:46:04 +0900

    trait を使って FactoryBot を DRY にした

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -59,21 +59,23 @@ RSpec.describe Project, type: :model do
     expect(project).to be_valid
   end
 
-  # 締め切り日が過ぎていれば遅延していること
-  it 'is late when the due date is past today' do
-    project = FactoryBot.create(:project_due_yesterday)
-    expect(project).to be_late
-  end
+  describe 'late status' do
+    # 締め切り日が過ぎていれば遅延していること
+    it 'is late when the due date is past today' do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
 
-  # 締め切り日が今日ならスケジュールどおりであること
-  it 'is on time when the due date is today' do
-    project = FactoryBot.create(:project_due_today)
-    expect(project).to_not be_late
-  end
+    # 締め切り日が今日ならスケジュールどおりであること
+    it 'is on time when the due date is today' do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
 
-  # 締め切り日が明日ならスケジュールどおりであること
-  it 'is on time when the due date is in the future' do
-    project = FactoryBot.create(:project_due_tomorrow)
-    expect(project).to_not be_late
+    # 締め切り日が明日ならスケジュールどおりであること
+    it 'is on time when the due date is in the future' do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
   end
 end
