480165fc876d9199b82c765f9e7c48003f1bf866
Ryu Sato <ryu@weseek.co.jp>
2019-07-18 02:36:36 +0900

    featuresテストで文字列を探す項目は一旦全て実行するように修正

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Projects', type: :feature do
   scenario 'user creates a new project' do
     user = FactoryBot.create(:user)
     sign_in user
-    
+
     visit root_path
 
     expect do
@@ -16,9 +16,11 @@ RSpec.feature 'Projects', type: :feature do
       fill_in 'Description', with: 'Trying out Capybara'
       click_button 'Create Project'
 
-      expect(page).to have_content 'Project was successfully created'
-      expect(page).to have_content 'Test Project'
-      expect(page).to have_content "Owner: #{user.name}"
+      aggregate_failures do
+        expect(page).to have_content 'Project was successfully created'
+        expect(page).to have_content 'Test Project'
+        expect(page).to have_content "Owner: #{user.name}"
+      end
     end.to change(user.projects, :count).by(1)
   end
 end
